5eddc897d5ae760139f4ffbfeaabbb70c34c699b
shagup1 <91088477+shagup1@users.noreply.github.com>
2021-09-22 10:34:43 -0700

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,9 @@
+a=int(input("enter how long you want to generate"))
+for num in range(1,a+1):
+    if(num%2!=0)
+        print(num,end=",")
+for num in range(1, ,2):
+    if(a==x):
+        print(num,end=",")
+print()
+              
