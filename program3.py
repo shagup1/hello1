f50c9be32809de12c34c764aa860a1f8ae73f39b
shagup1 <91088477+shagup1@users.noreply.github.com>
2021-09-22 10:54:40 -0700

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,15 @@
+a=int(input("any number:"))
+if(a==1)or(a==2):
+     print("1")
+elif(a==3)or(a==4):
+    for num in range(1,6,2):
+        print(num,end=",")
+elif(a==5)or(a==6):
+    for num in range(1,10,2):
+        print(num,end=",")
+elif(a=="x"):
+    for num in list[1: :2]:
+        print(num,end=",")
+    print()
+    
+    
